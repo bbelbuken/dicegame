e3c359d9292103d4a55e3d342ad70305880ecaef
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-09-23 23:32:07 +0300

    Add Roll Animation

--- main.js
+++ main.js
@@ -1,23 +1,55 @@
 let rollDice = () => {
-    let randomNumber1 = Math.floor(Math.random() * 6 + 1);
-    let randomNumber2 = Math.floor(Math.random() * 6 + 1);
-    
-    let randonImage1 = "images/dice" + randomNumber1 + ".png";
-    let randonImage2 = "images/dice" + randomNumber2 + ".png";
-    
-    let imgElements = document.querySelectorAll("img");
+  let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+  let randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
-    if (imgElements.length >= 2) {
-        imgElements[0].setAttribute("src", randonImage1);
-        imgElements[1].setAttribute("src", randonImage2);
-      }
-      
-      if (randomNumber1 === randomNumber2) {
-          document.querySelector("h1").innerHTML = "Draw!"
-      } else if (randomNumber1 > randomNumber2){
-          document.querySelector("h1").innerHTML = "🚩 Player 1 Wins "
-      } else {
-          document.querySelector("h1").innerHTML = "Player 2 Win 🚩"
-      }
-      
-}
+  let randomImage1 = "images/dice" + randomNumber1 + ".png";
+  let randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+  let imgElements = document.querySelectorAll("img");
+  let imgAnimation = document.querySelectorAll(".img1, .img2");
+
+
+  // after click random img event
+    let intervalID = setInterval(() => {
+        let newRndNbr1 = Math.floor(Math.random() * 6 + 1);
+        let newRndNbr2 = Math.floor(Math.random() * 6 + 1);
+
+        let randomImage1 = "images/dice" + newRndNbr1 + ".png";
+        let randomImage2 = "images/dice" + newRndNbr2 + ".png";
+
+        imgElements[0].setAttribute("src", randomImage1 )
+        imgElements[1].setAttribute("src", randomImage2 )
+    }, 200)
+
+
+
+  if (imgElements.length >= 2) {
+    imgAnimation.forEach((img) => {
+      img.style.animation = "rotate 0.2s linear infinite";
+      img.classList.add("filter-effect");
+    });
+    imgElements.forEach((elements) => {
+      elements.setAttribute("src", randomImage1, randomImage2);
+    });
+  }
+
+
+  setTimeout(() => {
+    imgElements[0].setAttribute("src", randomImage1);
+    imgElements[1].setAttribute("src", randomImage2);
+    if (randomNumber1 === randomNumber2) {
+      document.querySelector("h1").innerHTML = "Draw!";
+    } else if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "🚩 Player 1 Wins ";
+    } else {
+      document.querySelector("h1").innerHTML = "Player 2 Win 🚩";
+    }
+
+    imgAnimation.forEach((img) => {
+      img.style.animation = "none 0.5s linear infinite";
+      img.classList.remove("filter-effect");
+    });
+
+    clearInterval(intervalID)
+  }, 800);
+};
